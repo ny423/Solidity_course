515a7c64974a734eafc0004d6232b27931353c95
ny423 <91270086+ny423@users.noreply.github.com>
2022-01-10 22:06:12 +0800

    Create StorageFactory.sol

---
+++ StorageFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol"; // importing codes from another file
+
+// A is B: B is the parent class and A  is the child class
+contract StorageFactory is SimpleStorage {
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI (Application Binary Interface)
+        //this line has an explicit cast to the address type and initializes a new SimpleStorage object from the address
+        // Similar concept as getting the reference(&) of the object to change its value
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber); 
+
+        //this line simply gets the SimpleStorage object at the index _simpleStorageIndex in the array simpleStorageArray
+        //simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        //this line has an explicit cast to the address type and initializes a new SimpleStorage object from the address 
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve(); 
+
+        //this line simply gets the SimpleStorage object at the index _simpleStorageIndex in the array simpleStorageArray
+        //return simpleStorageArray[_simpleStorageIndex].retrieve(); 
+    }
+}
